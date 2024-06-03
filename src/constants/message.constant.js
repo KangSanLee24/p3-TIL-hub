6eb0f041ec5c31893da5c53b93a7113eba9f624d
GinGer-JSK <jskim5701@gmail.com>
2024-06-03 12:12:51 +0900

    에러 핸들러, constants(Message/HTTP) 일단 생성해봤습니다.

---
+++ src/constants/message.constant.js
@@ -0,0 +1,21 @@
+export const MESSAGES = {
+  AUTH: {
+    COMMON: {
+      UNAUTHORIZED: "인증 정보가 유효하지 않습니다.",
+      FORBIDDEN: "접근 권한이 없습니다.",
+      JWT: {
+        NO_TOKEN: "인증 정보가 없습니다.",
+        NOT_SUPPORTED_TYPE: "지원하지 않는 인증 방식입니다.",
+        EXPIRED: "인증 정보가 만료되었습니다.",
+        NO_USER: "인증 정보와 일치하는 사용자가 없습니다.",
+        INVALID: "인증 정보가 유효하지 않습니다.",
+        DISCARDED_TOKEN: "폐기 된 인증 정보입니다.",
+      },
+    },
+  },
+  USERS: {
+    READ_ME: {
+      SUCCEED: "내 정보 조회에 성공했습니다.",
+    },
+  },
+};
