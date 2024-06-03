825716a3a2f3f28feb6b595f583c3976ac011213
이강산 <gangsan1414@gmail.com>
2024-06-03 22:15:18 +0900

    ✨ update:CommentRouter추가

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import { SEVER_PORT } from "./constants/env.constant.js";
 import UserRouter from "./routers/user.router.js";
 import PostRouter from "./routers/post.router.js";
+import CommentRouter from "./routers/comment.router.js";
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(express.json()); //body에 있는거 json으로 바꾸는 기능
 app.use(express.urlencoded({ extended: true })); //form으로 들어오는 데이터를 body로 넘겨주는 기능
 app.use(cookieParser());
 app.use("/user", [UserRouter]);
-app.use("/post", [PostRouter]);
+app.use("/til", [PostRouter, CommentRouter]);
 
 app.listen(SEVER_PORT, () => {
   console.log(SEVER_PORT, "포트로 서버가 열렸어요!");
