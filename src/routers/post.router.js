fcb11ca88d0ee82146220140898ed75cfcb42eb1
이강산 <gangsan1414@gmail.com>
2024-06-05 17:04:08 +0900

    ✨ update:게시판목록조회API, Follower게시판목록조회API구현

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -2,7 +2,11 @@ import express from "express";
 import requireAccessToken from "../middlewares/require-access-token.middleware.js";
 import { prisma } from "../utils/prisma/index.js";
 import { postTIL } from "../middlewares/joi.js";
-import { requireRoles } from "../middlewares/require-roles.middleware.js";
+import {
+  requireListRoles,
+  requireDetailRoles,
+} from "../middlewares/require-roles.middleware.js";
+import { VISIBILITY } from "../constants/til.constant.js";
 
 const router = express.Router();
 
@@ -42,11 +46,33 @@ router.post("/", requireAccessToken, async (req, res, next) => {
 });
 
 // 게시글 목록 조회 API /til
-router.get("/", requireAccessToken, async (req, res, next) => {
+router.get("/", requireListRoles, async (req, res, next) => {
   try {
+    // 기본적으로 visibility가 PUBLIC인건 포함.
+    let whereCondition = {
+      OR: [{ visibility: VISIBILITY.PUBLIC }],
+    };
+
+    // 유저 정보가 있다면 MEMBER인지 MANAGER인지에 따라
+    //어떤 TIL visibility까지 접근가능한지
+    if (req.user) {
+      const { role } = req.user;
+      const userId = req.user.userId;
+
+      if (role === "MEMBER") {
+        whereCondition.OR.push({ UserId: +userId });
+      } else if (role === "MANAGER") {
+        whereCondition.OR.push(
+          { visibility: VISIBILITY.MANAGER },
+          { UserId: +userId }
+        );
+      }
+    }
+
     const sort = req.query.sort || "desc";
 
     const posts = await prisma.TIL.findMany({
+      where: whereCondition,
       orderBy: {
         createdAt: sort,
       },
@@ -62,7 +88,7 @@ router.get("/", requireAccessToken, async (req, res, next) => {
     });
 
     const response = posts.map((post) => ({
-      id: post.id,
+      tilId: post.tilId,
       userId: post.userId,
       userName: post.User.name,
       title: post.title,
@@ -84,6 +110,69 @@ router.get("/", requireAccessToken, async (req, res, next) => {
   }
 });
 
+// Follower 게시글 목록 조회 API /til/follower
+router.get("/follower", requireAccessToken, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+
+    //내가 팔로우 중인 사람 리스트 불러오기
+    const followees = await prisma.Follow.findMany({
+      where: { FollowerId: +userId },
+      select: { FolloweeId: true },
+    });
+
+    const followeeIds = followees.map((f) => f.FolloweeId);
+
+    let tils = [];
+    // followeeIds를 돌면서 TIL테이블을 돌아서 TILs를 가져온다.
+    for (const followeeId of followeeIds) {
+      const til = await prisma.TIL.findMany({
+        where: { visibility: "FOLLOWER", UserId: +followeeId },
+        include: {
+          User: {
+            select: {
+              userId: true,
+              name: true,
+            },
+          },
+          LikeLog: true,
+          Comment: true,
+        },
+      });
+      tils = tils.concat(til);
+    }
+
+    if (!tils) {
+      return res.status(400).json({
+        status: 400,
+        message: "게시글이 존재하지 않습니다.",
+      });
+    }
+
+    const response = tils.map((til) => ({
+      tilId: til.tilId,
+      userId: til.userId,
+      userName: til.User.name,
+      title: til.title,
+      content: til.content,
+      category: til.category,
+      visibility: til.visibility,
+      likeNumber: til.LikeLog.length,
+      comments: til.Comment,
+      createdAt: til.createdAt,
+      updatedAt: til.updatedAt,
+    }));
+
+    res.status(200).json({
+      status: 200,
+      message: "Follower의 게시글 목록 조회에 성공했습니다.",
+      data: response,
+    });
+  } catch (err) {
+    res.status(500).json({ status: 500, message: err.message });
+  }
+});
+
 // 게시물 수정 API /til/:id
 router.put("/:til_id", requireAccessToken, async (req, res, next) => {
   try {
@@ -95,6 +184,17 @@ router.put("/:til_id", requireAccessToken, async (req, res, next) => {
       return res
         .status(400)
         .json({ status: 400, errorMessage: "모든 필드를 입력해 주세요." });
+    }
+
+    // 접근가능한 userId, 입력받은 tilId랑 일치하는 TIL이 있는지 체크
+    const isExistTIL = await prisma.TIL.findFirst({
+      where: { tilId: +tilId, UserId: +userId },
+    });
+
+    if (!isExistTIL) {
+      return res.status(404).json({
+        errorMessage: "게시글이 존재하지 않거나 접근 권한이 없습니다.",
+      });
     }
 
     const post = await prisma.TIL.update({
@@ -119,10 +219,24 @@ router.put("/:til_id", requireAccessToken, async (req, res, next) => {
 
 // 게시물 삭제 API /til/:id
 router.delete("/:til_id", requireAccessToken, async (req, res, next) => {
-  const tilId = req.params.til_id;
-
   try {
-    await prisma.TIL.delete({ where: { tilId: parseInt(tilId) } });
+    const tilId = req.params.til_id;
+    const { userId } = req.user;
+
+    // 접근가능한 userId, 입력받은 tilId랑 일치하는 TIL이 있는지 체크
+    const isExistTIL = await prisma.TIL.findFirst({
+      where: { tilId: parseInt(tilId), UserId: +userId },
+    });
+
+    if (!isExistTIL) {
+      return res.status(404).json({
+        errorMessage: "게시글이 존재하지 않거나 접근 권한이 없습니다.",
+      });
+    }
+
+    await prisma.TIL.delete({
+      where: { tilId: parseInt(tilId), UserId: +userId },
+    });
     return res
       .status(200)
       .json({ status: 200, message: "게시글 삭제에 성공했습니다." });
@@ -135,7 +249,7 @@ router.delete("/:til_id", requireAccessToken, async (req, res, next) => {
 router.get(
   "/:til_id",
   requireAccessToken,
-  requireRoles,
+  requireDetailRoles,
   async (req, res, next) => {
     const params = req.params;
     const tilId = parseInt(params.til_id);
