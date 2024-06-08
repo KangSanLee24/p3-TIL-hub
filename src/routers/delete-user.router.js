68b3571a5e60ecb4e10cfd4c97045a0e53cb8bf9
ttt20222 <ju83144@gmail.com>
2024-06-08 22:48:16 +0900

    🩹 fix: 주석완료

--- src/routers/delete-user.router.js
+++ src/routers/delete-user.router.js
@@ -1,9 +1,12 @@
 import { prisma } from "../utils/prisma/index.js";
 
 //유효기간 3분 지나면 데이터 삭제
+//이메일 인증을 완료하지 않음
 export default async function deleteExpiredUsers() {
+  //현재 시간으로부터 3분전 시간 선언
   const threeMinutesAgo = new Date(Date.now() - 3 * 60 * 1000);
 
+  //디비에서 유저를 찾는데, createdAt시간이 3분전 보다 작은 것을 찾음
   const findUser = await prisma.User.findMany({
     where: {
         createdAt: {
@@ -15,6 +18,8 @@ export default async function deleteExpiredUsers() {
              name : true},
   });
 
+  //찾은 유저 배열 for문
+  //디비에서 유저 삭제함
   for (const user of findUser) {
     try {
       await prisma.$transaction(async (tx) => {
